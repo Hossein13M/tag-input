84ad0b02d157b86b27c0c2103ad2f9de82ecf8c5
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-03-31 20:39:25 +0430

    feat(tagChip): implement tag chip component

---
+++ src/app/components/tag-chip/tag-chip.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TagChipComponent } from './tag-chip.component';
+
+describe('TagChipComponent', () => {
+    let component: TagChipComponent;
+    let fixture: ComponentFixture<TagChipComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TagChipComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TagChipComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
