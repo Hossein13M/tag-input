121f21fc314a55b1cdefdbda0c31e67004cb08d8
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-04-01 00:28:49 +0430

    feat(input): enhance input component

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TagModel } from '../../models/tag.model';
 
 @Component({
@@ -7,7 +7,24 @@ import { TagModel } from '../../models/tag.model';
     styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
+    public inputValue: string = '';
     @Input() dataList: Array<TagModel> = [];
+    @Output() value: EventEmitter<string> = new EventEmitter<string>();
 
     constructor() {}
+
+    onTagValue(inputValue: string) {
+        this.value.emit(inputValue);
+        this.inputValue = '';
+    }
+
+    checkKey(event: KeyboardEvent) {
+        const validTagCreateKeys: Array<string> = ['Enter', 'Tab', ','];
+        if (validTagCreateKeys.includes(event.key)) {
+            if (event.key === ',') {
+                this.inputValue = this.inputValue.slice(0, -1);
+            }
+            this.onTagValue(this.inputValue);
+        }
+    }
 }
