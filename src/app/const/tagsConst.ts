b6a5cc1440bb376057ccc43079d3e56865ba2f34
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-04-01 13:46:57 +0430

    chore(global): implement suggestions list and its mock server

--- src/app/const/tagsConst.ts
+++ src/app/const/tagsConst.ts
@@ -1,6 +1,6 @@
 import { TagModel } from '../models/tag.model';
 
-export let TagsVariable: Array<TagModel> = [
+export const Suggestions: Array<TagModel> = [
     { id: 'apple', label: 'Apple' },
     { id: 'gillette', label: 'Gillette' },
     { id: 'mastercard', label: 'Mastercard' },
@@ -9,6 +9,24 @@ export let TagsVariable: Array<TagModel> = [
     { id: 'louis-vuitton', label: 'Louis Vuitton' },
 ];
 
+export let TagsVariable: Array<TagModel> = [
+    { id: 'javascript', label: 'JavaScript' },
+    { id: 'practice', label: 'Practice' },
+];
+
 export function updateTags(newTags: Array<TagModel>) {
     TagsVariable = newTags;
+}
+
+export function shuffleArray(array: Array<any>) {
+    let currentIndex = array.length,
+        randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
 }
