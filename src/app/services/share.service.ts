da71d37a1fce749e4aeb0c83c0352c8ad7220104
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-04-01 13:49:59 +0430

    feat(services): implement share service for sharing data between components

---
+++ src/app/services/share.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ShareService {
+    private hasBeenClickedInsideList: BehaviorSubject<any> = new BehaviorSubject(false);
+    private hasBeenClickedInsideInput: BehaviorSubject<any> = new BehaviorSubject(false);
+    private navigateWithinInput: BehaviorSubject<any> = new BehaviorSubject(null);
+    private hasPressedEnterOnEmptyInput: BehaviorSubject<any> = new BehaviorSubject(false);
+    private hasItemSelectedFromSuggestionWithKey: BehaviorSubject<any> = new BehaviorSubject(false);
+
+    constructor() {}
+
+    public hasClickedInsideList(): void {
+        this.hasBeenClickedInsideList.next(true);
+    }
+
+    public checkForClickStatusForList(): Observable<boolean> {
+        return this.hasBeenClickedInsideList.asObservable();
+    }
+
+    public hasClickedInsideInput(): void {
+        this.hasBeenClickedInsideInput.next(true);
+    }
+
+    public checkForClickStatusForInput(): Observable<boolean> {
+        return this.hasBeenClickedInsideInput.asObservable();
+    }
+
+    public hasNavigatedWithinTheInput(navDirection: 'ArrowDown' | 'ArrowUp'): void {
+        this.navigateWithinInput.next(navDirection);
+    }
+
+    public checkForNavigationWithinTheInput(): Observable<'ArrowDown' | 'ArrowUp'> {
+        return this.navigateWithinInput.asObservable();
+    }
+
+    public hasPressedEnterKeyOnEmptyInput(): void {
+        this.hasPressedEnterOnEmptyInput.next(true);
+    }
+
+    public checkForEnterKeyPressOnEmptyInput(): Observable<boolean> {
+        return this.hasPressedEnterOnEmptyInput.asObservable();
+    }
+
+    public hasItemBeenSelectedFromSuggestionList(): void {
+        this.hasItemSelectedFromSuggestionWithKey.next(true);
+    }
+
+    public checkForKeySelectionFromSuggestion(): Observable<boolean> {
+        return this.hasItemSelectedFromSuggestionWithKey.asObservable();
+    }
+}
